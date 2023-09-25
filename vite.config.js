bd75fef7159b420f90e0b95d320db9e19f49f15a
Cristobal Rivera Moreno <kris_todosinfin@outlook.com>
2023-09-24 21:42:33 -0600

    fix: :bug: Chnage url to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://github.com/Cristobal-Rivera-Moreno/Portafolio-Personal-Cris'
+  base : 'https://cristobal-rivera-moreno.github.io/Portafolio-Personal-Cris/'
 })
