5350af2f620f50f03de34efd1fae3af2111e3fac
Cristobal Rivera Moreno <kris_todosinfin@outlook.com>
2023-09-24 22:48:08 -0600

    fix(Responsive design): :bug: Change url to deploy.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://cristobal-rivera-moreno.github.io/Portafolio-Personal-Cris/'
+  base : 'https://github.com/cristobal-rivera-moreno/Portafolio-Personal-Cris/'
 })
