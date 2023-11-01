7e883409243eb29be742951beb243dd068ec58a1
Cristobal Rivera Moreno <kris_todosinfin@outlook.com>
2023-10-31 19:26:51 -0600

    feat: :sparkles: Certificate of go added.

--- src/ambito-educativo/Udemy.jsx
+++ src/ambito-educativo/Udemy.jsx
@@ -34,7 +34,7 @@ export const Udemy = ({children})=>{
                         <h2 className="inline-block">
                             Golang.
                         </h2>&nbsp;&nbsp;&nbsp;
-                        <a target="_blanck" className="inline-block absolute icon github   rounded-full" href="https://drive.google.com/file/d/1MAFQ8L33LJsCxpIIMvYjbrVbsxtqj19q/view?usp=sharing">
+                        <a target="_blanck" className="inline-block absolute icon github   rounded-full" href="https://drive.google.com/file/d/1-DjUcLjkVAP9hMyH8fPxS1rMkHgnTNmR/view?usp=sharing">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
                         </svg>
