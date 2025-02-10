0975b2cf7abad0fb1f3d0e373f0a0e7c1d662cf8
Cristobal Rivera <kris_todosinfin@outlook.com>
2025-02-09 22:55:42 -0600

    feat(Strings): Added some spanish strings.

---
+++ src/strings/spanish.jsx
@@ -0,0 +1,32 @@
+
+
+
+export const MSG = {
+    MSG_ID_1 : 'Cristobal Rivera Moreno. Ingeniero en sistemas computacionales.',
+    MSG_ID_2 : 'Ambito educativo',
+    MSG_ID_3 : 'Ambito laboral',
+    MSG_ID_4 : 'Ambito personal',
+    MSG_ID_5 : 'Menu',
+    MSG_ID_6 : 'Experiencia Laboral',
+    MSG_ID_7 : 'Proyectos Realizados',
+    MSG_ID_8 : 'Aspiraciones',
+    MSG_ID_9 : 'Experiencia Laboral',
+    MSG_ID_10 : 'Conocimiento',
+    MSG_ID_11 : 'Habilidades',
+    MSG_ID_12 : '👋 Hola, mi nombre es Cristobal Rivera Moreno, soy un apasionado por la tenología es por eso que mis aspiraciones se centran en seguirme desarrollando tanto personal como profesionalmente, aprendiendo de las experiencias y de los demás en el mundo tecnológico, y de esta forma apoyar a todo aquel que lo necesite a través de mis habilidades.\nEstoy conciente que las TIC\'s en la actualidad son de suma importancia es por eso que día a día trato de capacitarme con las nuevas novedades sobre un buen manejo de la computación, datos y seguridad de los mismos para seguir innovando en la sociedad.',
+    MSG_ID_13 : 'Ingeniero de Software',
+    MSG_ID_14 : 'Feb 2023 Grupo Sinco tech and business innnovationFeb 2023(actúal) Grupo Sinco tech and business innnovation',
+    MSG_ID_15 : 'Uso de tecnologias como Angular, C#,Laravel(PHP), J Query, javascript .NET, Sql Server y Postgres SQL para desarrollar aplicaciones.',
+    MSG_ID_16 : 'Asistente En Soporte Técnico (servicio social)',
+    MSG_ID_17 : 'Ago 2022 - Feb 2023 UAA departamento de redes',
+    MSG_ID_18 : 'Actividades Asignadas:',
+    MSG_ID_19 : '✅ Instalar paqueterias y equipos, como: Office, UAA apps y perifericos.\n✅ Apoyar a administrativos en el uso de tecnologias.\n✅ Diagnosticar problemas en sub-redes.\n✅ Mantenimiento a equipos de computo e impresoras.',
+    MSG_ID_20 : '✅ Analizar requerimientos.\n✅ Uso de SCRUM como framework ágil para desarrollo de sistemas.\n✅ Implementar y proponer funcionalidad basada en los requerimientos.\n✅ Administrar bases de datos.',
+    MSG_ID_21 : 'Desarrollador De Aplicaciones Mobiles Jr',
+    MSG_ID_22 : 'En mi estadia he participado en un proyecto destinado al control de aires acondicionados de la empresa Lennox, en el cual he sido miembro del equipo de desarrollo de la plataforma Android.\nUso de tecnologias como: Android Studio, Kotlin, Java, Azure DevOps, BLE (Bluetooth Low Energy) y Wifi Direct.',
+    MSG_ID_23 : '✅ Solucionar Bugs.\n✅ Implementar nuevas funcionalidades.\n✅ Analizar logs.\n✅ Estar en contacto con el equipo.\n✅ ',
+    MSG_ID_24 : 'Feb 2024 - Currently'
+    
+
+
+}
